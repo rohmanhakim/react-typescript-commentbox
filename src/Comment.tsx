61cd747ec31be3d5988b07f04c4c23d1ac2ea100
Muhammad Arif Rohman Hakim <rohmanhakim@live.com>
2016-08-21 16:35:44 +0700

    Removed Remarkable library

--- src/Comment.tsx
+++ src/Comment.tsx
@@ -1,17 +1,18 @@
 import * as React from "react"
-
+import * as Showdown from "showdown"
 interface CommentProps{
     author: string;
 }
 
 export class Comment extends React.Component<CommentProps,{}>{
+    converter: Showdown.Converter = new Showdown.Converter();
     public render(){
         return(
             <div className="comment">
                 <h2 className="commentAuthor">
                     {this.props.author}
                 </h2>
-                    {this.props.children}
+                {this.converter.makeHtml(this.props.children.toString())}
             </div>
         );
     }
